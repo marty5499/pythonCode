7d250c2066a19e8197693b4873b1499f4afa9308
marty <marty.shu@gmail.com>
2024-07-31 00:05:28 +0800

    update display

--- lib/webduino/display.py
+++ lib/webduino/display.py
@@ -1,22 +1,34 @@
 from machine import SPI, Pin
-from driver import st7735_buf
+from lib import st7735_buf
 from lib.easydisplay import EasyDisplay
 
 class Display:
-    def __init__(self, font, width=80, height=160, baudrate=40000000, polarity=0, phase=0,
+    def __init__(self, font="/text_lite_16px_2312.v3.bmf", width=80, height=160, baudrate=40000000, polarity=0, phase=0,
                  sck_pin=5, mosi_pin=3, cs_pin=4, dc_pin=2, res_pin=1, bl_pin=37,
                  rotate=3, invert=False, rgb=True, color=0xFFFF, clear=True):
         self.spi = SPI(2, baudrate=baudrate, polarity=polarity, phase=phase, sck=Pin(sck_pin), mosi=Pin(mosi_pin))
         self.dp = st7735_buf.ST7735(
             width=width, height=height, spi=self.spi,
             cs=Pin(cs_pin), dc=Pin(dc_pin), res=Pin(res_pin),
-            rotate=rotate, bl=Pin(bl_pin), invert=invert, rgb=rgb
+            rotate=rotate, bl=Pin(bl_pin), invert=True, rgb=rgb
         )
         self.ed = EasyDisplay(self.dp, "RGB565", font=font, show=True)
         self.color = color
         self.clear_screen = clear
         if clear:
             self.ed.clear()
+
+    def setColor(self, r, b, g):
+        # 將 0~100 的範圍轉換到 0~255
+        r = int(r * 255 / 100)
+        g = int(g * 255 / 100)
+        b = int(b * 255 / 100)
+        
+        # 轉換為 RGB565 格式
+        self.color = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
+
+    def bmp(self,img,x,y):
+        self.ed.bmp(img,x,y)
 
     def rect(self, x, y, w, h, color):
         self.ed.rect(x, y, w, h, color)
